66390a30d37c5139998d457cbe1708f128ae9803
unknown <jainamshah2718@gmail.com>
2021-01-06 23:05:45 +0530

    Authentication

--- backend/Authuser/views.py
+++ backend/Authuser/views.py
@@ -1,16 +1,26 @@
+# Django
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+
+# Rest Framework
 from rest_framework import status, generics, viewsets, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
-from Authuser.serializers import UserSerializer, VendorSerializer, AddressSerializer
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.authtoken.models import Token
 from rest_framework import serializers
-from Authuser.models import Vendors, Customers, User, Address
-import json
-from Authuser.permissions import IsOwner
-import logging
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+    HTTP_200_OK,
+)
 
+# Custom
+from Authuser.serializers import UserSerializer, VendorSerializer, AddressSerializer
+from Authuser.models import Vendors, Customers, User, Address
+from Authuser.permissions import IsOwner
+from Authuser.authentication import expires_in, is_token_expired, token_expire_handler, ExpiringTokenAuthentication
 # Create your views here.
 
 
@@ -59,6 +69,7 @@ def vendor_registration_view(request):
             data['email'] = user.email
             token = Token.objects.get(user=user).key
             data['token'] = token
+
         else:
             data = serializer1.errors
     else:
@@ -66,53 +77,59 @@ def vendor_registration_view(request):
     return Response(data)
 
 
-class AddressDetail(APIView):
+@api_view(["POST"])
+@permission_classes((AllowAny,))
+def signin_view(request):
 
-    def get_object(self, pk):
-        try:
-            address = Address.objects.get(pk=pk)
-            if address.user == request.user:
-                return address
-            else:
-                return None
-        except Snippet.DoesNotExist:
-            raise Http404
+    user = authenticate(
+        username=request.data['username'],
+        password=request.data['password']
+    )
+    if not user:
+        return Response({'detail': 'Invalid Credentials or activate account'}, status=HTTP_404_NOT_FOUND)
 
-    def get(self, request, pk, format=None):
-        address = self.get_object(pk)
-        if address != None:
-            serializer = AddressSerializer(address)
-        else:
-            return Response({'error': 'Bad Request'}, 400)
-        return Response(serializer.data)
+    # TOKEN STUFF
+    token, _ = Token.objects.get_or_create(user=user)
 
-    def put(self, request, pk, format=None):
-        address = self.get_object(pk)
-        serializer = AddressSerializer(address, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # token_expire_handler will check, if the token is expired it will generate new one
+    is_expired, token = token_expire_handler(token)
+    user_serialized = UserSerializer(user)
+    user_serialized.fields.pop('password')
 
-    def delete(self, request, pk, format=None):
-        address = self.get_object(pk)
-        address.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    return Response({
+        'user': user_serialized.data,
+        'expires_in': expires_in(token),
+        'token': token.key
+    }, status=HTTP_200_OK)
 
 
-class AddressList(APIView):
-    """
-    List all snippets, or create a new snippet.
-    """
+@api_view(["GET", "POST"])
+def testing(request):
+    print(request.user)
+    return Response()
 
-    def get(self, request, format=None):
-        address = Address.objects.all()
-        serializer = AddressSerializer(address, many=True)
-        return Response(serializer.data)
 
-    def post(self, request, format=None):
-        serializer = AddressSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(["GET"])
+@permission_classes((IsAuthenticated,))
+def logout(request):
+    Token.delete(request.auth)
+    return Response({
+        'message': 'Logged Out successfully'
+    }, status=HTTP_200_OK)
+
+
+class AddressViewSet(viewsets.ModelViewSet):
+    authentication_classes = (ExpiringTokenAuthentication,)
+    serializer_class = AddressSerializer
+    # get all products on DB
+    queryset = Address.objects.all()
+    permission_classes = (IsAuthenticated, )
+
+    def perform_create(self, serializer):
+        # when a product is saved, its saved how it is the owner
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        # after get all products on DB it will be filtered by its owner and return the queryset
+        owner_queryset = self.queryset.filter(user=self.request.user)
+        return owner_queryset
